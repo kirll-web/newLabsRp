c407968a216d70004af5bdade170028f22e8bca3
Reghan <reghansan@gmail.com>
2025-03-17 07:30:29 +0300

    изменения

--- scripts/start.bat
+++ scripts/start.bat
@@ -1,5 +1,5 @@
 
-cd /d "..\valuator"
+cd ..\valuator
 start "Valuator 5001" dotnet run --urls "http://0.0.0.0:5001"
 start "Valuator 5002" dotnet run --urls "http://0.0.0.0:5002"
 start "Valuator 5003" dotnet run --urls "http://0.0.0.0:5003"
